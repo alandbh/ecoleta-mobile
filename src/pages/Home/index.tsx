4bf916dc59daf3a4c76eff0e45a04c33ef4f9acc
Alan Vasconcelos <alan@MacBook-Pro-de-Alan-2.local>
2020-06-13 21:16:40 -0300

    Using Autocomplete Library

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,14 +1,53 @@
-import React from "react";
-import { View, ImageBackground, Text, Image, StyleSheet } from "react-native";
+import React, { useState, useEffect } from "react";
+import {
+    View,
+    ImageBackground,
+    Text,
+    Image,
+    StyleSheet,
+    TouchableOpacity,
+} from "react-native";
 import { RectButton } from "react-native-gesture-handler";
 import { Feather as Icon } from "@expo/vector-icons";
 import { useNavigation } from "@react-navigation/native";
+import Autocomplete from "react-native-autocomplete-input";
+import Axios from "axios";
 
 const logo = require("../../assets/logo.png");
 const homeBackground = require("../../assets/home-background.png");
 
+const fakeUfs = ["Rondônia", "Acre", "Amazonas", "Roraima"];
+
+interface UfResponse {
+    nome: string;
+}
+
 const Home: React.FC = () => {
     const navigation = useNavigation();
+    const [ufs, setUfs] = useState<String[]>([]);
+    const [filteredUfs, setFilteredUfs] = useState<String[]>([]);
+    const [selectedUf, setSelectedUf] = useState<string>("");
+
+    useEffect(() => {
+        Axios.get<UfResponse[]>(
+            "https://servicodados.ibge.gov.br/api/v1/localidades/estados"
+        ).then((response) => {
+            const ufsResponse = response.data.map((uf) => uf.nome);
+            setUfs(fakeUfs);
+            // console.log(ufsResponse);
+        });
+    }, []);
+
+    function handleSelectUf(text: string) {
+        console.log(text);
+        setSelectedUf(text);
+    }
+    function handleChangeText(queryTxt: string) {
+        console.log(queryTxt);
+        const queryUf =
+            queryTxt === "" ? [] : ufs.filter((uf) => uf.includes(queryTxt));
+        setFilteredUfs(queryUf);
+    }
 
     function handleNavigateToPoint() {
         navigation.navigate("Points");
@@ -19,7 +58,7 @@ const Home: React.FC = () => {
             style={styles.container}
             imageStyle={{ width: 274, height: 368 }}
         >
-            <View style={styles.main}>
+            <View style={{ ...styles.main, borderColor: "red" }}>
                 <Image source={logo} />
                 <Text style={styles.title}>
                     Seu marketplace de coleta de resíduos
@@ -28,6 +67,23 @@ const Home: React.FC = () => {
                     Ajudamos pessoas a encontrarem pontos de coleta de forma
                     eficiente.
                 </Text>
+            </View>
+            <View style={{ flex: 1 }}>
+                <Autocomplete
+                    style={styles.input}
+                    data={filteredUfs}
+                    defaultValue={selectedUf}
+                    onChangeText={(text) => handleChangeText(text)}
+                    renderItem={({ item }) => (
+                        <TouchableOpacity
+                            key={item as string}
+                            style={{ backgroundColor: "orange" }}
+                            onPress={() => handleSelectUf(item as string)}
+                        >
+                            <Text>{item}</Text>
+                        </TouchableOpacity>
+                    )}
+                />
             </View>
             <View style={styles.footer}>
                 <RectButton
@@ -78,6 +134,45 @@ const styles = StyleSheet.create({
     footer: {},
 
     select: {},
+
+    autocompleteContainer: {
+        flex: 1,
+        left: 0,
+        // position: "absolute",
+        right: 0,
+        top: 0,
+        zIndex: 1,
+        borderColor: "red",
+    },
+    itemText: {
+        fontSize: 15,
+        margin: 2,
+    },
+    descriptionContainer: {
+        // `backgroundColor` needs to be set otherwise the
+        // autocomplete input will disappear on text input.
+        backgroundColor: "#F5FCFF",
+        marginTop: 25,
+    },
+    infoText: {
+        textAlign: "center",
+    },
+    titleText: {
+        fontSize: 18,
+        fontWeight: "500",
+        marginBottom: 10,
+        marginTop: 10,
+        textAlign: "center",
+    },
+    directorText: {
+        color: "grey",
+        fontSize: 12,
+        marginBottom: 10,
+        textAlign: "center",
+    },
+    openingText: {
+        textAlign: "center",
+    },
 
     input: {
         height: 60,
